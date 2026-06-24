9ca18a2474bffc42dd9c0effff03c86d8e13df77
Abhirup Roy <abhiruproy2002@gmail.com>
2026-06-24 12:29:56 +0100

    fixing plotting script

--- plot_fluidn_curves.py
+++ plot_fluidn_curves.py
@@ -8,40 +8,32 @@
 Plotting the fluidisation curves
 """
 
-from prepost.plotting import ProbeAnalysis
+from prepost import FlBedPlot
 import pandas as pd
 
 
-pressure_path = 'CFD/postProcessing/cuttingPlane/'
-velcfg_path = 'prepost/velcfg.txt'
+pressure_path = "CFD/postProcessing/cuttingPlane/"
+velcfg_path = "prepost/velcfg.txt"
 
 
-probe_cfdem_slices = ProbeAnalysis(
-    pressure_path=pressure_path,
-    nprobes=5,
-    velcfg_path=velcfg_path,
-    dump2csv=False
+probe_cfdem_slices = FlBedPlot(
+    pressure_path=pressure_path, nprobes=5, velcfg_path=velcfg_path, dump2csv=False
 )
 
 probe_cfdem_slices.plot_pressure(
-    slice_dirn="z", 
-    x_var="velocity", 
+    slice_dirn="z",
+    x_var="velocity",
     png_name="pressure_vel_plot_z",
     use_slices=True,
-    dump_probe0=True
+    dump_probe0=True,
 )
 
 probe_cfdem_slices.plot_voidfrac(
-    slice_dirn="y", 
-    x_var="velocity",
-    png_name="voidfrac_vel_plot_y",
-    dump_probe0=True
+    slice_dirn="y", x_var="velocity", png_name="voidfrac_vel_plot_y", dump_probe0=True
 )
 
-probe_cfdem_slices.plot_pressure(slice_dirn="z",
-    x_var="time", 
-    png_name="pressure_time_plot_z",
-    use_slices=True
+probe_cfdem_slices.plot_pressure(
+    slice_dirn="z", x_var="time", png_name="pressure_time_plot_z", use_slices=True
 )
 
 # probe_cfdem_slices.plot_contactarea()
